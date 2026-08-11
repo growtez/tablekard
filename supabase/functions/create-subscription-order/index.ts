6d4763c90b38069bedfb79bd090dc9041c73654f
Faruk Khan <faruk.khan.cse@gmail.com>
2026-08-11 05:34:48 +0530

    Razorpay error fixed

--- supabase/functions/create-subscription-order/index.ts
+++ supabase/functions/create-subscription-order/index.ts
@@ -115,7 +115,7 @@ serve(async (req: Request) => {
             .from("restaurant_users")
             .select("id")
             .eq("restaurant_id", restaurant_id)
-            .eq("profile_id", user.id)
+            .eq("auth_user_id", user.id)
             .eq("active", true)
             .maybeSingle();
 
